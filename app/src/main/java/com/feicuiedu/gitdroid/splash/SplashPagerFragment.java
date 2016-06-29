8c7da62d7252c8f860a09d6a56a423fb0f3664d8
liuqun <905169916@qq.com>
2016-06-29 15:57:57 +0800

    Splash页面完成，加入Splash页面上手机(layoutPhone)的一些动画处理

--- app/src/main/java/com/feicuiedu/gitdroid/splash/SplashPagerFragment.java
+++ app/src/main/java/com/feicuiedu/gitdroid/splash/SplashPagerFragment.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import com.feicuiedu.gitdroid.R;
 import com.feicuiedu.gitdroid.splash.pager.Pager0;
@@ -36,6 +37,12 @@ public class SplashPagerFragment extends Fragment {
     int             colorYellow;
     @Bind(R.id.content)
     FrameLayout     frameLayout;
+    @Bind(R.id.ivPhoneBlank)
+    ImageView       mIvPhoneBlank;
+    @Bind(R.id.ivPhone)
+    ImageView       mIvPhone;
+    @Bind(R.id.layoutPhone)
+    FrameLayout     mLayoutPhone;
 
     private SplashPagerAdapter mAdapter;
 
@@ -43,6 +50,7 @@ public class SplashPagerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_splash_pager, container, false);
+        ButterKnife.bind(this, view);
         return view;
     }
 
@@ -54,12 +62,50 @@ public class SplashPagerFragment extends Fragment {
 
         mAdapter = new SplashPagerAdapter(view.getContext());
         mViewPager.setAdapter(mAdapter);
+        //监听页面,添加属性动画以及YoYo动画
         mViewPager.addOnPageChangeListener(mPageChangeListener);
+        //监听页面,添加mLayoutPhone的平移,缩放,渐变效果
+        mViewPager.addOnPageChangeListener(phoneChangeListener);
         mIndicator.setViewPager(mViewPager);
 
         Pager0 pager0 = (Pager0) mAdapter.getView(0);
         pager0.showAnimation();
     }
+//此监听主要负责viewpager在scroll过程中,当前布局上mLayoutPhone布局的平移,缩放,渐变的处理
+    private final ViewPager.OnPageChangeListener phoneChangeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            //viewPager在第一和第二个页面之间
+            if (position == 0) {
+                //移动过程中实时scale(缩放)
+                float scale = 0.3f + positionOffset * 0.7f;
+                mLayoutPhone.setScaleX(scale);
+                mLayoutPhone.setScaleY(scale);
+
+                //在移动过程中,有一个平移的动画
+                int scroll = (int) ((positionOffset-1)*400);
+                mLayoutPhone.setTranslationX(scroll);
+                //在移动过程中,alph实时变化
+                mIvPhone.setAlpha(positionOffset);
+                return;
+            }
+            //当ViewPager在第二和第三个页面之间时(总是为1),手机要和ViewPager一起平移
+            if (position == 1) {
+                mLayoutPhone.setTranslationX(-positionOffsetPixels);
+                return;
+            }
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
 
     private final ViewPager.OnPageChangeListener mPageChangeListener = new ViewPager.OnPageChangeListener() {
         //ARGB取值器(属性动画)
@@ -87,7 +133,7 @@ public class SplashPagerFragment extends Fragment {
 
         @Override
         public void onPageSelected(int position) {
-            if (position ==2 ){
+            if (position == 2) {
                 Pager2 pager2 = (Pager2) mAdapter.getView(2);
                 pager2.showAnimation();
             }
@@ -98,4 +144,10 @@ public class SplashPagerFragment extends Fragment {
 
         }
     };
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
 }
