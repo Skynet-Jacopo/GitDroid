e259bde981b70b589720169c4e87c7deb49e8722
liuqun <905169916@qq.com>
2016-06-29 10:51:54 +0800

    增加了属性动画

--- app/src/main/java/com/feicuiedu/gitdroid/splash/SplashPagerFragment.java
+++ app/src/main/java/com/feicuiedu/gitdroid/splash/SplashPagerFragment.java
@@ -1,5 +1,6 @@
 package com.feicuiedu.gitdroid.splash;
 
+import android.animation.ArgbEvaluator;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,12 +8,13 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import com.feicuiedu.gitdroid.R;
 
 import butterknife.Bind;
+import butterknife.BindColor;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 import me.relex.circleindicator.CircleIndicator;
 
 /**
@@ -24,6 +26,14 @@ public class SplashPagerFragment extends Fragment {
     ViewPager       mViewPager;
     @Bind(R.id.indicator)
     CircleIndicator mIndicator;
+    @BindColor(R.color.colorGreen)
+    int             colorGreen;
+    @BindColor(R.color.colorRed)
+    int             colorRed;
+    @BindColor(R.color.colorYellow)
+    int             colorYellow;
+    @Bind(R.id.content)
+    FrameLayout     frameLayout;
 
     private SplashPagerAdapter mAdapter;
 
@@ -42,24 +52,42 @@ public class SplashPagerFragment extends Fragment {
 
         mAdapter = new SplashPagerAdapter(view.getContext());
         mViewPager.setAdapter(mAdapter);
+        mViewPager.addOnPageChangeListener(mPageChangeListener);
         mIndicator.setViewPager(mViewPager);
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        ButterKnife.unbind(this);
-    }
+    private final ViewPager.OnPageChangeListener mPageChangeListener = new ViewPager.OnPageChangeListener() {
+        //ARGB取值器(属性动画)
+        final ArgbEvaluator mEvaluator = new ArgbEvaluator();
 
-    @OnClick({R.id.viewPager, R.id.indicator})
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.viewPager:
-                break;
-            case R.id.indicator:
-                break;
-            
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            //当前scroll 的比例 :positionOffset
+            //start颜色:做一个
+            //end 颜色:自己做一个
+            if (position == 0) {
+                int color = (int) mEvaluator.evaluate(positionOffset,
+                        colorGreen, colorRed);
+                frameLayout.setBackgroundColor(color);
+                return;
+            }
+            if (position == 1) {
+                int color = (int) mEvaluator.evaluate(positionOffset,
+                        colorRed, colorYellow);
+                frameLayout.setBackgroundColor(color);
+                return;
+            }
 
         }
-    }
+
+        @Override
+        public void onPageSelected(int position) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
 }
