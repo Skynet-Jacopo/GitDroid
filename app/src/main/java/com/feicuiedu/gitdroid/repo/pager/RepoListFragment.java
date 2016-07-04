aaf37096730b334c1b738e3e9980a2765baa6c31
Skynet-Jacopo <905169916@qq.com>
2016-07-04 11:26:17 +0800

    使用mugen,实现上拉加载,重新分了一下包,使用MVP框架模式,直接使用Mosby库,使项目结构更简单清晰

--- app/src/main/java/com/feicuiedu/gitdroid/repo/pager/RepoListFragment.java
+++ app/src/main/java/com/feicuiedu/gitdroid/repo/pager/RepoListFragment.java
@@ -2,20 +2,27 @@ package com.feicuiedu.gitdroid.repo.pager;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.feicuiedu.gitdroid.R;
+import com.feicuiedu.gitdroid.components.FooterView;
+import com.feicuiedu.gitdroid.presenter.ReopListPresenter;
+import com.feicuiedu.gitdroid.view.PtrPageView;
+import com.hannesdorfmann.mosby.mvp.MvpFragment;
+import com.mugen.Mugen;
+import com.mugen.MugenCallbacks;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import in.srain.cube.views.ptr.PtrClassicFrameLayout;
 import in.srain.cube.views.ptr.PtrDefaultHandler;
 import in.srain.cube.views.ptr.PtrFrameLayout;
@@ -23,18 +30,21 @@ import in.srain.cube.views.ptr.PtrFrameLayout;
 /**
  * Created by liuqun on 6/30/2016.
  */
-public class RepoListFragment extends Fragment {
+public class RepoListFragment extends MvpFragment<PtrPageView,ReopListPresenter> implements
+        PtrPageView {
 
 
     @Bind(R.id.lvRepos)
     ListView              mLvRepos;
     @Bind(R.id.ptrClassicFrameLayout)
     PtrClassicFrameLayout mPtrClassicFrameLayout;
+    @Bind(R.id.emptyView)
+    TextView              mEmptyView;
+    @Bind(R.id.errorView)
+    TextView              mErrorView;
 
     private ArrayAdapter<String> mAdapter;
-    private List<String> datas = new ArrayList<>();
-
-    private static int mCount;
+    private FooterView           footerView; // 上拉加载更多的视图
 
     public static RepoListFragment getInstance(String language) {
         RepoListFragment fragment = new RepoListFragment();
@@ -42,15 +52,6 @@ public class RepoListFragment extends Fragment {
         bundle.putSerializable("key_language", language);
         fragment.setArguments(bundle);
         return fragment;
-    }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-//        for (int i = 0; i < 20; i++) {
-//            mCount++;
-//            datas.add("我是第" + mCount + "条数据");
-//        }
     }
 
     @Nullable
@@ -61,6 +62,11 @@ public class RepoListFragment extends Fragment {
     }
 
     @Override
+    public ReopListPresenter createPresenter() {
+        return new ReopListPresenter();
+    }
+
+    @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         ButterKnife.bind(this, view);
@@ -68,45 +74,41 @@ public class RepoListFragment extends Fragment {
         mAdapter = new ArrayAdapter<String>(getContext(), android.R.layout
                 .simple_list_item_1);
         mLvRepos.setAdapter(mAdapter);
-        mAdapter.addAll(datas);
 
         mPtrClassicFrameLayout.setPtrHandler(new PtrDefaultHandler() {
             @Override
             public void onRefreshBegin(PtrFrameLayout frame) {
-                loadData(20);
+                getPresenter().loadData();
             }
         });
-    }
 
-    private void loadData(int size) {
-        new Thread(new Runnable() {
+        footerView = new FooterView(getContext());
+        // 上拉加载更多(listview滑动动最后的位置了，就可以loadmore)
+        Mugen.with(mLvRepos, new MugenCallbacks() {
             @Override
-            public void run() {
-                //只是测试
-                try {
-                    datas.clear();
-                    Thread.sleep(3000);
-                    for (int i = 0; i < 20; i++) {
-                        mCount++;
-                        datas.add("我是第" + mCount + "条数据");
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                //到UI线程中完成视图工作
-                mPtrClassicFrameLayout.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        mAdapter.clear();
-                        mAdapter.addAll(datas);
-                        mAdapter.notifyDataSetChanged();
-                        mPtrClassicFrameLayout.refreshComplete();
-                    }
-                });
+            public void onLoadMore() {
+                Toast.makeText(getContext(), "loadmore", Toast.LENGTH_SHORT).show();
+                getPresenter().loadMore();
+            }
+
+            // 是否正在加载，此方法用来避免重复加载
+            @Override
+            public boolean isLoading() {
+                return mLvRepos.getFooterViewsCount() > 0 && footerView.isLoading();
+            }
+
+            // 是否所有数据都已加载
+            @Override
+            public boolean hasLoadedAllItems() {
+                return mLvRepos.getFooterViewsCount() > 0 && footerView.isComplete();
             }
         }).start();
     }
 
+    @OnClick({R.id.emptyView, R.id.errorView})
+    public void autoRefresh() {
+        mPtrClassicFrameLayout.autoRefresh();
+    }
 
     @Override
     public void onDestroyView() {
@@ -114,4 +116,77 @@ public class RepoListFragment extends Fragment {
         ButterKnife.unbind(this);
     }
 
+    // 这是上拉加载更多视图层实现------------------------------------------------------
+    @Override
+    public void addMoreData(List<String> datas) {
+        mAdapter.addAll(datas);
+    }
+
+    @Override
+    public void hideLoadMore() {
+        mLvRepos.removeFooterView(footerView);
+    }
+
+    @Override
+    public void showLoadMoreLoading() {
+        if (mLvRepos.getFooterViewsCount() == 0) {
+            mLvRepos.addFooterView(footerView);
+        }
+        footerView.showLoading();
+    }
+
+    @Override
+    public void showLoadMoreErro(String msg) {
+        if (mLvRepos.getFooterViewsCount() == 0) {
+            mLvRepos.addFooterView(footerView);
+        }
+        footerView.showError(msg);
+    }
+
+    @Override
+    public void showLoadMoreEnd() {
+        if (mLvRepos.getFooterViewsCount() == 0) {
+            mLvRepos.addFooterView(footerView);
+        }
+        footerView.showComplete();
+    }
+
+    // 这是下拉刷新视图的实现-------------------------------------------------------
+    @Override
+    public void showContentView() {
+        mPtrClassicFrameLayout.setVisibility(View.VISIBLE);
+        mEmptyView.setVisibility(View.GONE);
+        mErrorView.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void showErroView(String msg) {
+        mPtrClassicFrameLayout.setVisibility(View.GONE);
+        mEmptyView.setVisibility(View.GONE);
+        mErrorView.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void showEmptyView() {
+        mPtrClassicFrameLayout.setVisibility(View.GONE);
+        mEmptyView.setVisibility(View.VISIBLE);
+        mErrorView.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void refreshData(List<String> strings) {
+        mAdapter.clear();
+        mAdapter.addAll(strings);
+        mAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void stopRefresh() {
+        mPtrClassicFrameLayout.refreshComplete();
+    }
+
+    @Override
+    public void showMessage(String msg) {
+        Toast.makeText(getContext(), msg, Toast.LENGTH_SHORT).show();
+    }
 }
